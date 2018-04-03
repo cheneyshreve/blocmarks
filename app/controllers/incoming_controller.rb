d5e29fe6896bac9827dbf119bc15046d90a09dbe
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-03 16:24:14 -0700

    mods for creating new topic

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -20,9 +20,11 @@ class IncomingController < ApplicationController
 
     if @topic.nil?
       @topic = Topic.create(title: params[:subject], user_id: @user)
+      @topic.save!
     end
 
     @bookmark = @topic.bookmarks.create(url: @url, user_id: @user.id)
+
 
     head 200
   end
