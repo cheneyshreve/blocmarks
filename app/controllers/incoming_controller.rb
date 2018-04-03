60e121ffcfe522b022103ea1e79a30f3d85ac0d8
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-03 16:07:57 -0700

    updates to try to fix mailgun

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,7 +1,7 @@
 class IncomingController < ApplicationController
 
   skip_before_action :verify_authenticity_token, only: [:create]
-  skip_before_action :authenticate_user
+  skip_before_action :authenticate_user!
 
   def create
 
