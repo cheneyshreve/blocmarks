f7b403a74133a121dcda997ed43faf0af14f11cc
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-03 16:46:31 -0700

    still debugging issues with topiccreation via mail

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -20,6 +20,7 @@ class IncomingController < ApplicationController
 
     if @topic.nil?
       @topic = Topic.create(title: params[:subject], user_id: @user)
+      @topic.save!
     end
 
     @bookmark = @topic.bookmarks.create(url: @url, user_id: @user.id)
