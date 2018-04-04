ae53d018b4e5a5d3232ceafa6ad71d5ffdae5792
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-04 09:47:24 -0700

    still debugging topics

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -17,7 +17,8 @@ class TopicsController < ApplicationController
 
   def create
     @topic = Topic.new(topic_params)
-    @topic.user = current_user
+    @topic.user = User.find_by(params[:email])
+    # @topic.user = current_user
 
     if @topic.save
       redirect_to @topic, notice: "Topic was saved successfully!"
