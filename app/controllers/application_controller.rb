6d719d3e7b491246a420a5d07881c7efe91684cc
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-04 16:02:46 -0700

    working on view mods for pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,14 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
-    private
+    protected
     def user_not_authorized
       flash[:alert] = "You are not authorized to perform this action."
       redirect_to root_path
+    end
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :name
     end
 
 end
