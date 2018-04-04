0516edaf12f5535d4aedbbe57f63496ecb24c2cd
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-04-04 11:26:27 -0700

    debugging

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -1,9 +1,5 @@
 class IncomingController < ApplicationController
 
-  skip_before_action :verify_authenticity_token!, only: [:create]
-  # skip_before_action :authenticate_user!, only: [:create]
-  # with above line, works fine for adding exisiting bookmarks, but not for new topics,then user not found
-
   def create
     byebug
     @user =  User.find_by(email: params[:sender])
