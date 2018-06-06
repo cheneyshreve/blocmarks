647c3bbc7712d613d730f6d069b278d6e419aafd
cheneyshreve <cheneyshreve@users.noreply.github.com>
2018-06-06 08:37:19 -0700

    udpated seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_id = 1
+topic_id = 1
+Topic.create!(title: "code stuff", user_id: 1)
+Bookmark.create!(topic_id: 7, user_id: 1, url: "geekwire.com")
+Bookmark.create!(topic_id: 7, user_id: 1, url: "codecademy.com")
+Bookmark.create!(topic_id: 7, user_id: 1, url: "udacity.com")
+Bookmark.create!(topic_id: 7, user_id: 1, url: "bloc.io")
+Bookmark.create!(topic_id: 7, user_id: 1, url: "leetcode.com")
